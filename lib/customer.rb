377aff0e4c9198976b67be02a4ce798ec2cd110b
Zachary Schulz <zjschulz@gmail.com>
2019-10-13 17:48:45 +0000

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,32 @@
 class Customer
   
+  attr_reader :name, :age
+  
+  @@all = []
+  
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def new_meal(waiter, total, tip)
+    Meal.new(waiter, self, total, tip)
+  end
+  
+  def meals
+    Meal.all.select do |x|
+      x if x.customer.name == self.name
+    end
+  end
+  
+  def waiters
+    meals.map do |x| x.waiter
+    end
+  end
+  
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,31 @@
 class Waiter
 
+  attr_reader :name, :years
+  
+  @@all = []
+  
+  def initialize(name, years)
+    @name = name
+    @years = years
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def new_meal(customer, total, tip)
+    Meal.new(self, customer, total, tip)
+  end
+  
+  def meals
+    Meal.all.select do |x|
+      x if x.waiter.name == self.name
+    end
+  end
+  
+  def best_tipper
+    meals.sort_by {|x| x.tip}[-1].customer
+  end
+  
 end
