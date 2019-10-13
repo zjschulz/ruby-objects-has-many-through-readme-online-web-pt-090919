3086adec6d3b3e4d9f00fa6ec86ff0e1659caad5
Zachary Schulz <zjschulz@gmail.com>
2019-10-13 17:39:05 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,20 @@
 class Meal
 
+  attr_accessor :waiter, :customer
+  attr_reader :total, :tip 
+  
+  @@all =[]
+  
+  def initialize(waiter, customer, total, tip)
+    @waiter = waiter
+    @customer = customer
+    @total = total
+    @tip = tip
+    @@all << self
+  end
+
+  def self.all
+    @@all  
+  end 
+
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,17 @@
 class Waiter
 
+  attr_reader :name, :years
+  
+  @@all = []
+  
+  def initialize(name, years)
+    @name = name
+    @years = years
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
 end
